6c657ba2c5d9316c4595619e6539a0dd4c8a6884
Swayam Yadav <97746128+swayamyadav05@users.noreply.github.com>
2026-05-21 18:11:22 +0530

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        mp = defaultdict(int)
+
+        for i in range(len(numbers)):
+            diff = target - numbers[i]
+            if mp[diff] and mp[diff] != i+1:
+                return [mp[diff], i + 1]
+            mp[numbers[i]] = i + 1
+        return []
+
+# TC = O(n)
+# SC = O(n)
